dcd9f7da6d9fd9351315fb3e058805b4585fe0e4
Joshua Gray <joshgray91@gmail.com>
2025-01-28 09:21:33 +0000

    implement subtract in main.js

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ function add(num1, num2){
     return num1 + num2;
 }
 
+function subtract(num1, num2){
+    return num1 + num2;
+}
+
 let number_1 = 5;
 let number_2 = 7;
 
@@ -14,3 +18,4 @@ console.log("============Main.js============");
 
 console.log("Square of 4 is: " + square(4));
 console.log(number_1.toString() + " + " +  number_2.toString() + " is: " + add(number_1, number_2));
+console.log(number_1.toString() + " - " +  number_2.toString() + " is: " + subtract(number_1, number_2));
